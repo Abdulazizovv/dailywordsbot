4bd401a52df625ba23a1238cb06b004deb75a613
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2024-12-11 12:57:03 +0500

    created app  and integrated

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'botapp'
+    'rest_framework',
+    'botapp',
+    'api',
 ]
 
 MIDDLEWARE = [
