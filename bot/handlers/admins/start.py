f349f04930f83f81872dc1f9517e9bfeb54e7f38
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2024-12-14 20:32:40 +0500

    updated bot user adding to make request to api

--- bot/handlers/admins/start.py
+++ bot/handlers/admins/start.py
@@ -3,9 +3,20 @@ from bot.loader import dp
 from bot.filters import IsAdmin
 from aiogram.dispatcher.filters import Command
 from bot.keyboards.inline.main_menu import main_menu_kb
+from bot.utils.requests_ import create_user
 
 
 @dp.message_handler(Command("start"), IsAdmin())
 async def bot_start(message: types.Message):
+    
+    create_user(
+        {
+        "user_id":message.from_user.id,
+        "username":message.from_user.username,
+        "first_name":message.from_user.first_name,
+        "last_name":message.from_user.last_name
+        }
+    )
+    
     await message.answer(f"Hello {message.from_user.full_name}\n", reply_markup=main_menu_kb)
 
--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -1,20 +1,29 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from bot.loader import dp
-from bot.utils.db_api import add_bot_user
-from asgiref.sync import sync_to_async
+from bot.keyboards.inline import main_menu_kb as menu_kb
+from bot.utils.requests_ import create_user
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await sync_to_async(add_bot_user)(
-        user_id=message.from_user.id, 
-        first_name=message.from_user.first_name, 
-        last_name=message.from_user.last_name,
-        username=message.from_user.username
-        )
+    
+    # creating user if not exists
+    # if user exists return user
+    user = create_user(
+        {
+        "user_id":message.from_user.id,
+        "username":message.from_user.username,
+        "first_name":message.from_user.first_name,
+        "last_name":message.from_user.last_name
+        }
+    )
+
+    if not user:
+        return await message.answer("Nimadir xato ketdi. Iltimos qaytadan urinib ko'ring /start buyrug'ini bosing.")
     # salomlashish xabari
-    await message.answer(f"Salom, {message.from_user.id}!")
+    await message.answer(f"Assalomu alaykum {message.from_user.full_name}!\n"
+                         "Siz bosh menyudasiz!", reply_markup=menu_kb)
 
 
 
